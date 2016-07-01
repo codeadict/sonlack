99c2b263fc72250773617f99f616e906fd5fbf2f
Dairon Medina <dairon.medina@gmail.com>
2016-07-01 15:19:53 -0400

    Check that settings are set before running

--- robot.py
+++ robot.py
@@ -1,11 +1,17 @@
+import sys
 import logging
 from slackbot.bot import Bot
+import slackbot_settings as conf
 
 logger = logging.getLogger('sonlack')
 
 
 def main():
     """Then main Bot application"""
+    if not conf.API_TOKEN or not conf.SPEAKER_IP:
+        print('Slack API token or Sonos speaker IP not set.\n'
+              'Please add configure it before running Sonlack. Exiting...')
+        sys.exit(0)
     bot = Bot()
     print('Sonlack is rocking now. Enjoy your favorite music...')
     bot.run()
